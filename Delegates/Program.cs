c08f5ccd85a37aeb72f52cfdd104917acd468bb5
Miguel Fernandez <miguelrf95@gmail.com>
2019-11-13 16:28:12 +0000

    Add delegate exercise

---
+++ Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            StcConcat sc = new StcConcat("hollow words written here");
+
+            Action<string> gAs = ToUpper;
+            gAs += ToLower;
+            gAs += sc.ConcatAndPrint;
+
+            gAs("This is a text but,");
+
+        }
+        public static void ToUpper(string text)
+        {
+            Console.WriteLine(text.ToUpper());
+        }
+
+        public static void ToLower(string text)
+        {
+            Console.WriteLine(text.ToLower());
+        }
+    }
+}
---
+++ Delegates/StcConcat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    public struct StcConcat
+    {
+        private string myStr;
+
+        public StcConcat(string str)
+        {
+            myStr = str;
+        }
+
+        public void ConcatAndPrint(string text)
+        {
+            Console.WriteLine(text + myStr);
+        }
+    }
+}
